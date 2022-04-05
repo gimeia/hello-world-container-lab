b9950628b7e11f25dbcda46071e1aec5602ae5e1
Evan A Slatis <eslatis@redhat.com>
2022-04-05 14:09:38 -0500

    simple test app

--- src/main/java/org/hippy/actiontest/ActiontestApplication.java
+++ src/main/java/org/hippy/actiontest/ActiontestApplication.java
@@ -1,10 +1,25 @@
 package org.hippy.actiontest;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActiontestApplication {
+public class ActiontestApplication implements CommandLineRunner {
+
+    @Value("${message.welcome}")
+    private String welcomeMsg;
+
+    @Override
+    public void run(String args[]) throws Exception {
+        while (true) {
+            System.out.println(welcomeMsg);
+            TimeUnit.SECONDS.sleep(3);
+        }   
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActiontestApplication.class, args);
